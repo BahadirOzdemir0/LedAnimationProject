6e88df5b6d2dbe5a5d60d8ede467ec47991433dd
Bahadır Özdemir <126866965+BahadirOzdemir0@users.noreply.github.com>
2024-07-16 22:41:01 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -7,7 +7,14 @@
 #include <time.h>
 
 // Function prototypes
-void ledlight(uint32_t delay_ms);
+void Transition();
+void LedAnime1(uint32_t delay_ms);
+void LedAnime2(uint32_t delay_ms);
+void LedAnime3(uint32_t delay_ms);
+void LedAnime4(uint32_t delay_ms);
+
+
+
 void delay(uint32_t delay_ms);
 
 // Function to create a delay using NOP (No Operation) instructions
@@ -23,17 +30,22 @@ uintptr_t *pClkReg = (uintptr_t*)0x40023830;
 uintptr_t *pPortDModeReg = (uintptr_t*)0x40020c00;
 uintptr_t *pPortDOutReg = (uintptr_t*)0x40020c14;
 
+void Transition(){
+	*pPortDOutReg |= 0x1ff0;
+	delay(100);
+	*pPortDOutReg &= 0x00000000;
+}
 // Function to control LED light animation
-void ledlight(uint32_t delay_ms) {
+void LedAnime1(uint32_t delay_ms) {
     // LED animation loop
-    for(int i = 0; i < 9; i++) {
+    for(int8_t i = 0; i < 9; i++) {
         *pPortDOutReg &= 0x00000000;            // Clear PC bits
         *pPortDOutReg |= 0x1000 >> i;           // Set portD 12 to 4 bit for LED control
         delay(delay_ms);              // Delay for smooth animation
     }
 
     // LED animation in reverse
-    for(int i = 8; i >= 0; i--) {
+    for(int8_t i = 8; i >= 0; i--) {
         *pPortDOutReg &= 0x00000000;            // Clear PC bits
         *pPortDOutReg |= 0x1000 >> i;           // Set portD 12 to 4 bit for LED control
         delay(delay_ms);              // Delay for smooth animation
@@ -41,7 +53,32 @@ void ledlight(uint32_t delay_ms) {
 
     *pPortDOutReg &= 0x00000000;                // Turn off all LEDs
 }
+void LedAnime2(uint32_t delay_ms){
+	for(int8_t i=0;i<5;i++){
+		*pPortDOutReg &= 0x00000000;
 
+		*pPortDOutReg |= 0x0100 <<i;
+		*pPortDOutReg |= 0x0100 >>i;
+		delay(delay_ms);
+
+	}
+	 *pPortDOutReg &= 0x00000000;
+}
+void LedAnime3(uint32_t delay_ms){
+	for(int8_t i=4;i>=0;i--){
+			*pPortDOutReg &= 0x00000000;
+
+			*pPortDOutReg |= 0x0100 <<i;
+			*pPortDOutReg |= 0x0100 >>i;
+			delay(delay_ms);
+
+		}
+		 *pPortDOutReg &= 0x00000000;
+}
+void LedAnime4(uint32_t delay_ms){
+	LedAnime2(20);
+	LedAnime3(20);
+}
 int main(void) {
     *pClkReg |= 0x08;                      // Enable peripheral A
     *pPortDModeReg &= 0x00000000;                // Clear PB bits
@@ -49,9 +86,41 @@ int main(void) {
 
 
 
-    for(;;){        // Infinite loop for continuous operation
+    for(int8_t choice=1;choice <7;choice++){        // Infinite loop for continuous operation
 
 
-        ledlight(10);           // Determine delay based on user
+    	//LedAnime1(10);      // Determine delay based on user
+
+
+    	switch(choice){
+    		case 1:
+    			Transition();
+    			LedAnime1(10);
+    			LedAnime1(10);
+    			LedAnime1(10);
+    			break;
+    		case 2:
+    			Transition();
+    			LedAnime2(20);
+    			LedAnime2(20);
+    			LedAnime2(20);
+    			break;
+    		case 3:
+    			Transition();
+    			LedAnime3(20);
+    			LedAnime3(20);
+    			LedAnime3(20);
+    			break;
+    		case 4:
+    			Transition();
+    			LedAnime4(20);
+    			LedAnime4(20);
+    			LedAnime4(20);
+    			break;
+    		default:
+    			choice=0;
+
+    	}
+
     }
 }
